dca75b4b55ac858d7879a8be664de885e7c2d917
webfaisalbd <web.faisal.bd@gmail.com>
2023-09-15 14:31:48 +0600

    reset

--- index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-const config = require("./config/config")
-
-const PORT = config.app.port;
-
-app.listen(PORT, ()=>{
-    console.log(`app is running at http://localhost:${PORT}`);
-})
